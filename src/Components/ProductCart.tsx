b69633ecb3511489cfd3012fb865d06aadd303d9
Anton Rum <antonrumancev67@gmail.com>
2025-03-24 00:57:18 +0300

    added buttons

--- src/Components/ProductCart.tsx
+++ src/Components/ProductCart.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import s from "../Styles/Components/ProductCart.module.css";
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
 
 interface ProductCartProps {
     title: string;
@@ -7,10 +9,18 @@ interface ProductCartProps {
 }
 
 const ProductCart = ({ title, description, image }:ProductCartProps ) => {
+  const [ isLiked, setIsLiked ] = useState<boolean>(false);
+  
   return (
     <div className={s.cartBox}>
         <img className={s.productImage} src={image} alt='Cart image' />
+        <div className={s.btn_box}>
+          <button className={s.del_btn}>🗑️</button>
+          <button className={s.like_btn} 
+                  onClick={() => setIsLiked(!isLiked)}>{isLiked ? <FaHeart /> : <FaRegHeart /> }</button>
+        </div>
         <p className={s.title}>{title}</p>
+        <button className={s.moreInfoBtn}>Больше информации</button>
     </div>
   )
 }
